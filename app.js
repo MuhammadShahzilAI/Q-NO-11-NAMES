93b44635b21b5c4d64ab312cc3a3fc88d2be83fa
MUHAMMAD SHAHZIL <164368568+MuhammadShahzilAI@users.noreply.github.com>
2024-03-22 16:09:41 +0500

    Q 11 NAMES
    ASSIGMENT OF GOVERNER SINDH

---
+++ app.js
@@ -0,0 +1,11 @@
+//Exercise 11
+//let members : string [] =[`Atif`,`Danish`,`Shahmeer`,`Kinza`,`Mubashara`];
+//for( let i =0; i<members.length; i++)
+//console.log (members[i]);
+//}
+// Exercise 12
+var members = ["Atif", "Danish", "Shahmeer", "Kinza", "Mubashara"];
+var message = "kal birthay hai kis ki:";
+for (var i = 0; i < members.length; i++) {
+    console.log(message + members[i]);
+}
---
+++ app.ts
@@ -0,0 +1,13 @@
+//Exercise 11
+//let members : string [] =[`Atif`,`Danish`,`Shahmeer`,`Kinza`,`Mubashara`];
+//for( let i =0; i<members.length; i++)
+ //console.log (members[i]);
+
+//}
+// Exercise 12
+let members : string [] =[`Atif`,`Danish`,`Shahmeer`,`Kinza`,`Mubashara`];
+let message : string= `kal birthay hai kis ki:`;
+for( let i =0; i<members.length; i++){
+  console.log ( message + members[i]);
+}
+ 
